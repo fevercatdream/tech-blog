0d5639982c0e214ce761e96c8caa11fe8ed3ca94
fevercatdream <kuma18@uw.edu>
2023-05-14 22:18:33 -0700

    have blog posts and comments display as read only until update button selected

--- public/js/profile.js
+++ public/js/profile.js
@@ -23,13 +23,14 @@ document.querySelector("form").addEventListener("submit",e=>{
 // front end route update blog post
 const userBlogPosts = document.querySelectorAll(".user-blog-post");
 for(let userBlogPost of userBlogPosts) {
-    userBlogPost.querySelector(".post-update-btn").addEventListener("click", e=>{
+    // event listener if save blog post button clicked
+    userBlogPost.querySelector(".save-btn").addEventListener("click", e=>{
         e.preventDefault();
         const blogPostObj = {
             title:userBlogPost.querySelector(".update-title").value,
             content:userBlogPost.querySelector(".update-post").value,
         }
-        fetch("/api/blogpost/" + e.target.dataset.blogpostUpdateId,{
+        fetch("/api/blogpost/" + e.target.dataset.blogpostId,{
             method:"PUT",
             body:JSON.stringify(blogPostObj),
             headers:{
@@ -43,17 +44,22 @@ for(let userBlogPost of userBlogPosts) {
             }
         })
     })
+    // event listener if update blog post button clicked
+    userBlogPost.querySelector(".post-update-btn")?.addEventListener("click", () => updateBlogContent(userBlogPost));
+    // event listener if cancel blog post button clicked
+    userBlogPost.querySelector(".cancel-btn")?.addEventListener("click", () => cancelUpdateBlogContent(userBlogPost));
 }
 
 // front end route update comment
 const userComments = document.querySelectorAll(".user-comment");
 for(let userComment of userComments) {
-    userComment.querySelector(".comment-update-btn").addEventListener("click", e=>{
+    // event listener if save comment button clicked
+    userComment.querySelector(".comment-save-btn").addEventListener("click", e=>{
         e.preventDefault();
         const commentObj = {
             comment:userComment.querySelector(".update-comment").value,
         }
-        fetch("/api/comment/" + e.target.dataset.commentUpdateId,{
+        fetch("/api/comment/" + e.target.dataset.commentId,{
             method:"PUT",
             body:JSON.stringify(commentObj),
             headers:{
@@ -67,6 +73,10 @@ for(let userComment of userComments) {
             }
         })
     })
+    // event listener if update comment button clicked
+    userComment.querySelector(".comment-update-btn")?.addEventListener("click", () => updateCommentContent(userComment));
+    // event listener if cancel comment button clicked
+    userComment.querySelector(".comment-cancel-btn")?.addEventListener("click", () => cancelUpdateCommentContent(userComment));
 }
 
 // front end route delete blog post
@@ -102,3 +112,52 @@ allCommentDelBtns.forEach(button=>{
         })
     })
 })
+
+// function to update blog post if update button selected
+function updateBlogContent(userBlogPost){
+    userBlogPost.querySelector(".post-update-btn").classList.add("hidden");
+    userBlogPost.querySelector(".save-btn").classList.remove("hidden");
+    userBlogPost.querySelector(".cancel-btn").classList.remove("hidden");
+    userBlogPost.querySelector(".update-title-ro").classList.add("hidden");
+    userBlogPost.querySelector(".update-title").classList.remove("hidden");
+    userBlogPost.querySelector(".update-title").removeAttribute("readonly");
+    userBlogPost.querySelector(".update-post-ro").classList.add("hidden");
+    userBlogPost.querySelector(".update-post").classList.remove("hidden");
+    userBlogPost.querySelector(".update-post").removeAttribute("readonly");
+}
+
+// function to cancel changing a blog post if cancel button selected
+function cancelUpdateBlogContent(userBlogPost){
+    userBlogPost.querySelector(".post-update-btn").classList.remove("hidden");
+    userBlogPost.querySelector(".save-btn").classList.add("hidden");
+    userBlogPost.querySelector(".cancel-btn").classList.add("hidden");
+    userBlogPost.querySelector(".update-title-ro").classList.remove("hidden");
+    userBlogPost.querySelector(".update-title").classList.add("hidden");
+    userBlogPost.querySelector(".update-title").setAttribute("readonly", "");
+    userBlogPost.querySelector(".update-post-ro").classList.remove("hidden");
+    userBlogPost.querySelector(".update-post").classList.add("hidden");
+    userBlogPost.querySelector(".update-post").setAttribute("readonly", "");
+    location.reload()    
+}
+
+// function to update comment if update button selected
+function updateCommentContent(userComment){
+    userComment.querySelector(".comment-update-btn").classList.add("hidden");
+    userComment.querySelector(".comment-save-btn").classList.remove("hidden");
+    userComment.querySelector(".comment-cancel-btn").classList.remove("hidden");
+    userComment.querySelector(".update-comment-ro").classList.add("hidden");
+    userComment.querySelector(".update-comment").classList.remove("hidden");
+    userComment.querySelector(".update-comment").removeAttribute("readonly");
+}
+
+// function to cancel changing a comment if cancel button selected
+function cancelUpdateCommentContent(userComment){
+    userComment.querySelector(".comment-update-btn").classList.remove("hidden");
+    userComment.querySelector(".comment-save-btn").classList.add("hidden");
+    userComment.querySelector(".comment-cancel-btn").classList.add("hidden");
+    userComment.querySelector(".update-comment-ro").classList.remove("hidden");
+    userComment.querySelector(".update-comment").classList.add("hidden");
+    userComment.querySelector(".update-comment").setAttribute("readonly", "");
+    location.reload()    
+}
+
--- public/js/singleBlogPost.js
+++ public/js/singleBlogPost.js
@@ -1,7 +1,10 @@
+// blog post variables
 const postUpdateBtn = document.querySelector(".post-update-btn");
 const saveBtn = document.querySelector(".save-btn");
 const cancelBtn = document.querySelector(".cancel-btn");
+const updatePostTitleHRO = document.querySelector(".update-title-ro");
 const updatePostTitle = document.querySelector(".update-title");
+const updatePostTextPRO = document.querySelector(".update-post-ro");
 const updatePostTextArea = document.querySelector(".update-post");
 
 // front end route get single blog post
@@ -29,7 +32,7 @@ document.querySelector("#comment_form").addEventListener("submit",e=>{
 // front end route update blog post
 const userBlogPost = document.querySelector(".user-blog-post");
 
-saveBtn.addEventListener("click", e=>{
+saveBtn?.addEventListener("click", e=>{
     e.preventDefault();
     const blogPostObj = {
         title:userBlogPost.querySelector(".update-title").value,
@@ -53,12 +56,13 @@ saveBtn.addEventListener("click", e=>{
 // front end route update comment
 const userComments = document.querySelectorAll(".user-comment");
 for(let userComment of userComments) {
-    userComment.querySelector(".comment-update-btn")?.addEventListener("click", e=>{
+    // event listener if save comment button clicked
+    userComment.querySelector(".comment-save-btn")?.addEventListener("click", e=>{
         e.preventDefault();
         const commentObj = {
             comment:userComment.querySelector(".update-comment").value,
         }
-        fetch("/api/comment/" + e.target.dataset.commentUpdateId,{
+        fetch("/api/comment/" + e.target.dataset.commentId,{
             method:"PUT",
             body:JSON.stringify(commentObj),
             headers:{
@@ -72,6 +76,10 @@ for(let userComment of userComments) {
             }
         })
     })
+    // event listener if update comment button clicked
+    userComment.querySelector(".comment-update-btn")?.addEventListener("click", () => updateCommentContent(userComment));
+    // event listener if cancel comment button clicked
+    userComment.querySelector(".comment-cancel-btn")?.addEventListener("click", () => cancelUpdateCommentContent(userComment));
 }
 
 // front end route delete blog post
@@ -108,22 +116,55 @@ allCommentDelBtns.forEach(button=>{
     })
 })
 
+// function to update blog post if update button selected
 function updateBlogContent(){
     postUpdateBtn.classList.add("hidden");
     saveBtn.classList.remove("hidden");
     cancelBtn.classList.remove("hidden");
+    updatePostTitleHRO.classList.add("hidden");
+    updatePostTitle.classList.remove("hidden");
     updatePostTitle.removeAttribute("readonly");
+    updatePostTextPRO.classList.add("hidden");
+    updatePostTextArea.classList.remove("hidden");
     updatePostTextArea.removeAttribute("readonly");
 }
 
+// function to cancel changing a blog post if cancel button selected
 function cancelUpdateBlogContent(){
     postUpdateBtn.classList.remove("hidden");
     saveBtn.classList.add("hidden");
     cancelBtn.classList.add("hidden");
+    updatePostTitleHRO.classList.remove("hidden");
+    updatePostTitle.classList.add("hidden");
     updatePostTitle.setAttribute("readonly", "");
+    updatePostTextPRO.classList.remove("hidden");
+    updatePostTextArea.classList.add("hidden");
     updatePostTextArea.setAttribute("readonly", "");
     location.reload()    
 }
 
+// function to update comment if update button selected
+function updateCommentContent(userComment){
+    userComment.querySelector(".comment-update-btn").classList.add("hidden");
+    userComment.querySelector(".comment-save-btn").classList.remove("hidden");
+    userComment.querySelector(".comment-cancel-btn").classList.remove("hidden");
+    userComment.querySelector(".update-comment-ro").classList.add("hidden");
+    userComment.querySelector(".update-comment").classList.remove("hidden");
+    userComment.querySelector(".update-comment").removeAttribute("readonly");
+}
+
+// function to cancel changing a comment if cancel button selected
+function cancelUpdateCommentContent(userComment){
+    userComment.querySelector(".comment-update-btn").classList.remove("hidden");
+    userComment.querySelector(".comment-save-btn").classList.add("hidden");
+    userComment.querySelector(".comment-cancel-btn").classList.add("hidden");
+    userComment.querySelector(".update-comment-ro").classList.remove("hidden");
+    userComment.querySelector(".update-comment").classList.add("hidden");
+    userComment.querySelector(".update-comment").setAttribute("readonly", "");
+    location.reload()    
+}
+
+// event listener if blog update button clicked
 postUpdateBtn.addEventListener("click", updateBlogContent);
+// event listener if blog cancel button clicked
 cancelBtn.addEventListener("click", cancelUpdateBlogContent);
